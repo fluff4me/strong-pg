be65c5fe6498e3036af29a49aa22520b10d9d9e5
Chiri Vulpes <yuudaari@gmail.com>
2022-02-14 14:49:59 +1100

    Delete node_modules/ first

--- tasks/meta.ts
+++ tasks/meta.ts
@@ -1,4 +1,3 @@
-import { spawn } from "child_process";
 import fs from "mz/fs";
 import Task from "./utilities/Task";
 
@@ -14,17 +13,17 @@ export default Task("meta", async () => {
 	await fs.copyFile("LICENSE", "build/LICENSE");
 	await fs.copyFile("README.md", "build/README.md");
 
-	await fs.writeFile("build/.gitignore", `
-		node_modules/
-	`.split("\n").map(path => path.trim()).filter(path => path).join("\n") + "\n");
+	// await fs.writeFile("build/.gitignore", `
+	// 	node_modules/
+	// `.split("\n").map(path => path.trim()).filter(path => path).join("\n") + "\n");
 
-	await new Promise<void>((resolve, reject) => {
-		const ext = process.platform === "win32" ? ".cmd" : "";
-		const childProcess = spawn("npm" + ext, ["install"], { cwd: "build", stdio: [process.stdin, process.stdout, process.stderr] });
-		childProcess.on("error", reject);
-		childProcess.on("exit", code => {
-			if (code === 1) reject("Error code 1");
-			else resolve();
-		});
-	});
+	// await new Promise<void>((resolve, reject) => {
+	// 	const ext = process.platform === "win32" ? ".cmd" : "";
+	// 	const childProcess = spawn("npm" + ext, ["install"], { cwd: "build", stdio: [process.stdin, process.stdout, process.stderr] });
+	// 	childProcess.on("error", reject);
+	// 	childProcess.on("exit", code => {
+	// 		if (code === 1) reject("Error code 1");
+	// 		else resolve();
+	// 	});
+	// });
 });
