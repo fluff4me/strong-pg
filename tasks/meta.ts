5a0bd3dd45f07e8d7778df12890da42ff3ff92cb
Chiri Vulpes <yuudaari@gmail.com>
2022-02-14 14:48:37 +1100

    Build: .gitignore node_modules/

--- tasks/meta.ts
+++ tasks/meta.ts
@@ -14,6 +14,10 @@ export default Task("meta", async () => {
 	await fs.copyFile("LICENSE", "build/LICENSE");
 	await fs.copyFile("README.md", "build/README.md");
 
+	await fs.writeFile("build/.gitignore", `
+		node_modules/
+	`.split("\n").map(path => path.trim()).filter(path => path).join("\n") + "\n");
+
 	await new Promise<void>((resolve, reject) => {
 		const ext = process.platform === "win32" ? ".cmd" : "";
 		const childProcess = spawn("npm" + ext, ["install"], { cwd: "build", stdio: [process.stdin, process.stdout, process.stderr] });
