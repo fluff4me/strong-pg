bf8bb00c4e5cc25cba6d56f40bb87d22301303e7
Chiri Vulpes <6081834+ChiriVulpes@users.noreply.github.com>
2022-03-31 12:30:31 +1100

    Only allow dropping everything if env var set

--- src/Database.ts
+++ src/Database.ts
@@ -21,8 +21,13 @@ export default class Database<SCHEMA extends DatabaseSchema> {
 
 	/**
 	 * @deprecated WARNING: This drops and recreates your database!
+	 * 
+	 * Does nothing if `DEBUG_PG_ALLOW_DROP` is not set in your env.
 	 */
 	public async drop () {
+		if (!process.env.DEBUG_PG_ALLOW_DROP)
+			return;
+
 		return this.pool.query("DROP OWNED BY CURRENT_USER CASCADE");
 	}
 }
