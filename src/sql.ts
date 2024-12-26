6367a3fe370acb79b34faff0c64b2fc9facacfd9
Chiri Vulpes <chiri@chiri.works>
2024-12-26 16:43:04 +1300

    Allow sql`` as an interpolation in sql``

--- src/sql.ts
+++ src/sql.ts
@@ -1,8 +1,68 @@
 import { QueryConfig } from "pg"
 
-export function sql (segments: TemplateStringsArray, ...interpolations: any[]): QueryConfig {
-	let text = segments[0]
-	for (let i = 0; i < interpolations.length; i++)
-		text += `$${i + 1}${segments[i + 1]}`
-	return { text, values: interpolations }
+const SYMBOL_SQL = Symbol("Sql")
+
+type SqlTemplateData = [segments: TemplateStringsArray, interpolations: unknown[]]
+interface Sql extends QueryConfig {
+	[SYMBOL_SQL]: SqlTemplateData
+}
+
+function isSql (value: unknown): value is Sql {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+	return typeof value === "object" && !!value && !!(value as Sql)[SYMBOL_SQL]
+}
+
+export function sql (segments: TemplateStringsArray, ...interpolations: unknown[]): QueryConfig {
+	if (!interpolations.length)
+		return { text: segments[0] }
+
+	const data: SqlTemplateData = [segments, interpolations];
+	const result: Sql = {
+		[SYMBOL_SQL]: data,
+		get text () {
+			compile(data)
+			return this.text
+		},
+		get values () {
+			compile(data)
+			return this.values
+		},
+	}
+
+	return result
+
+	function compile ([segments, interpolations]: SqlTemplateData) {
+		if (!interpolations.length)
+			return { text: segments[0] }
+
+		let resultInterpolations: unknown[] | undefined
+
+		let vi = 1
+		const text = recurse()
+
+		Object.defineProperty(result, "text", { value: text })
+		Object.defineProperty(result, "values", { value: resultInterpolations ?? interpolations })
+
+		function recurse (recursiveData?: SqlTemplateData) {
+			if (recursiveData?.[1].length)
+				resultInterpolations = data[1].slice()
+
+			const [segments, interpolations] = recursiveData ?? data
+
+			let text = segments[0]
+			for (let i = 0; i < interpolations.length; i++) {
+				const interpolation = interpolations[i]
+
+				if (isSql(interpolation)) {
+					text += recurse(interpolation[SYMBOL_SQL])
+					continue
+				}
+
+				resultInterpolations?.push(interpolation)
+				text += `$${vi++}${segments[i + 1]}`
+			}
+
+			return text
+		}
+	}
 }
