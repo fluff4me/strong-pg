7070d431757b30770b7419bd01095c92d2a6671e
Lumina Mystere <minimotogo@gmail.com>
2025-01-02 20:17:28 +1300

    Fix function return type incorrectly being never

--- src/FunctionCall.ts
+++ src/FunctionCall.ts
@@ -6,16 +6,27 @@ import { VirtualTable } from "./VirtualTable";
 export type FunctionOutput<SCHEMA extends DatabaseSchema, FUNCTION extends FunctionSchema, FUNCTION_NAME extends DatabaseSchema.FunctionName<SCHEMA>> =
 	FUNCTION extends FunctionSchema<(TypeString | OptionalTypeString)[], infer OUT, infer RETURN> ?
 	{ [I in keyof OUT as OUT[I] extends [TypeString, infer NAME extends PropertyKey] ? NAME : never]: OUT[I] extends [infer TYPE extends TypeString, string] ? TYPE : never } extends infer OUT_COLUMNS ?
-	RETURN extends `SETOF ${infer TABLE_NAME extends DatabaseSchema.TableName<SCHEMA>}` ?
-	DatabaseSchema.Table<SCHEMA, TABLE_NAME> extends infer TABLE ?
+	(
+		(
+			RETURN extends `SETOF ${infer TABLE_NAME extends DatabaseSchema.TableName<SCHEMA>}` ?
+			(DatabaseSchema.Table<SCHEMA, TABLE_NAME> extends infer TABLE ? TABLE : never)
 
-	OUT["length"] extends 0 ? TABLE
-	: { [KEY in keyof TABLE | keyof OUT_COLUMNS]: KEY extends keyof OUT_COLUMNS ? OUT_COLUMNS[KEY] : KEY extends keyof TABLE ? Extract<TABLE[KEY], TypeString> : never }
+			: RETURN extends `SETOF ${infer TYPE_NAME extends DatabaseSchema.TypeName<SCHEMA>}` ?
+			(DatabaseSchema.Type<SCHEMA, TYPE_NAME> extends infer TYPE ? TYPE : never)
 
+			: never
+		) extends infer TABLE ?
+
+		[TABLE] extends [never] ?
+		(OUT["length"] extends 0 ? { [KEY in FUNCTION_NAME]: RETURN } : "Error! Function return type must be a table if there are OUT parameters")
+		: (
+			OUT["length"] extends 0 ? TABLE
+			: { [KEY in keyof TABLE | keyof OUT_COLUMNS]: KEY extends keyof OUT_COLUMNS ? OUT_COLUMNS[KEY] : KEY extends keyof TABLE ? Extract<TABLE[KEY], TypeString> : never }
+		)
+		: never
+	)
 	: never
-	: (OUT["length"] extends 0 ? { [KEY in FUNCTION_NAME]: RETURN } : "Error! Function return type must be a table if there are OUT parameters")
-	: never
-	: never
+	: never;
 
 class FunctionCall<FUNCTION extends FunctionSchema, SCHEMA extends DatabaseSchema, FUNCTION_NAME extends DatabaseSchema.FunctionName<SCHEMA>, OUTPUT extends TableSchema = Extract<FunctionOutput<SCHEMA, FUNCTION, FUNCTION_NAME>, TableSchema>> extends VirtualTable<OUTPUT, never> {
 
