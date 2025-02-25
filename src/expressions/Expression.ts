f098fca4f30234569de2e88135d3d20bc54b12a0
Chiri Vulpes <chiri@chiri.works>
2025-02-25 23:26:18 +1300

    Fix expr.notExists not working correctly, add expr.exists

--- src/expressions/Expression.ts
+++ src/expressions/Expression.ts
@@ -47,6 +47,7 @@ export interface ExpressionValues<VARS = never, VALUE = null, RESULT = null> {
 	currentValue (sequenceId: string): ExpressionOperations<VARS, number>;
 	true: ExpressionOperations<VARS, boolean>;
 	false: ExpressionOperations<VARS, boolean>;
+	exists<DATABASE_SCHEMA extends DatabaseSchema, TABLE extends DatabaseSchema.TableName<DATABASE_SCHEMA>> (database: Database<DATABASE_SCHEMA>, table: TABLE, initialiser: NoInfer<Initialiser<SelectFromVirtualTable<JoinTables<"INNER", Extract<VARS, TableSchema>, DatabaseSchema.Table<DATABASE_SCHEMA, TABLE>, never, TABLE>, never, 1>>>): ExpressionOperations<VARS, boolean>;
 	notExists<DATABASE_SCHEMA extends DatabaseSchema, TABLE extends DatabaseSchema.TableName<DATABASE_SCHEMA>> (database: Database<DATABASE_SCHEMA>, table: TABLE, initialiser: NoInfer<Initialiser<SelectFromVirtualTable<JoinTables<"INNER", Extract<VARS, TableSchema>, DatabaseSchema.Table<DATABASE_SCHEMA, TABLE>, never, TABLE>, never, 1>>>): ExpressionOperations<VARS, boolean>;
 }
 
@@ -270,6 +271,9 @@ export default class Expression<VARS = never> implements ImplementableExpression
 	}
 
 	private innerValue (value: ExpressionOr<VARS, ValidType>, mapper?: (value: string) => string) {
+		if (value as never === this)
+			return this
+
 		this.parts.push(() => {
 			const stringified = Expression.stringifyValue(value as ValidType | ExpressionInitialiser<VARS>, this.vars, this.enableStringConcatenation);
 			return mapper ? mapper(stringified) : stringified;
@@ -306,11 +310,21 @@ export default class Expression<VARS = never> implements ImplementableExpression
 		return this;
 	}
 
-	public notExists (database: Database<any>, table: string, initialiser: Initialiser<SelectFromVirtualTable<any, string, 1>>) {
-		const select = database.table(table).select()
+	public exists (database: Database<any>, table: string, initialiser: Initialiser<SelectFromVirtualTable<any, string, 1>>) {
+		const select = database.table(table).select(1)
 		select["vars"] = this.vars;
 		initialiser(select as never);
-		this.parts.push(() => `NOT EXISTS (${select.compile()[0].text})`);
-		return this;
+		const e = new Expression(this.vars, this.enableStringConcatenation);
+		e.parts.push(() => `EXISTS (${select.compile()[0].text})`);
+		return e;
+	}
+
+	public notExists (database: Database<any>, table: string, initialiser: Initialiser<SelectFromVirtualTable<any, string, 1>>) {
+		const select = database.table(table).select(1)
+		select["vars"] = this.vars;
+		initialiser(select as never);
+		const e = new Expression(this.vars, this.enableStringConcatenation);
+		e.parts.push(() => `NOT EXISTS (${select.compile()[0].text})`);
+		return e;
 	}
 }
