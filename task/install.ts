01d251b2f6c23758d5827353cdd5f62751eece6b
Chiri Vulpes <chiri@chiri.works>
2025-11-30 20:34:10 +1300

    Update to task install dependencies/devDependencies separation

--- task/install.ts
+++ task/install.ts
@@ -3,7 +3,7 @@ import { Task } from 'task'
 export default Task('install', async task => task.install(
 	{
 		path: '.',
-		dependencies: {
+		devDependencies: {
 			task: { repo: 'chirivulpes/task', branch: 'package' },
 			lint: { repo: 'fluff4me/lint' },
 		},
